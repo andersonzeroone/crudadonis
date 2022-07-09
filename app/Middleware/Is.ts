bfbe47bc6041f00c4ad352d39f12c795f2e01739
Anderson Pablo <anderson.pablo02@gmail.com>
2022-07-09 18:26:32 -0300

    create middleware ath

--- app/Middleware/Is.ts
+++ app/Middleware/Is.ts
@@ -1,8 +1,33 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import User from 'App/Models/User'
 
 export default class Is {
-  public async handle({}: HttpContextContract, next: () => Promise<void>) {
+  public async handle(
+    { auth, response }: HttpContextContract,
+    next: () => Promise<void>,
+    guards?: string[]
+  ) {
     // code for middleware goes here. ABOVE THE NEXT CALL
-    await next()
+
+    const userId = await auth.user?.id
+    let isNext = false
+
+    if (userId && guards) {
+      const user = await User.query().where('id', userId).preload('roles').first()
+
+      const userJson = user?.serialize()
+
+      userJson?.roles.forEach(({ name }) => {
+        guards.forEach((nameRolesGuards) => {
+          if (name.toLowerCase() === nameRolesGuards.toLowerCase()) {
+            isNext = true
+          }
+        })
+      })
+    }
+
+    if (isNext) return next()
+
+    return response.forbidden({ message: 'Your user aren´t authorized' })
   }
 }
